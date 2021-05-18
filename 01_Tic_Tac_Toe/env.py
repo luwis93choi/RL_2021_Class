0ad86a1257622a2090f58a30a4fc4a6cc7d4ac30
luwis93choi <luwis93choi@hotmail.com>
2021-05-18 19:03:29 +0900

    Added step function for environment

--- 01_Tic_Tac_Toe/env.py
+++ 01_Tic_Tac_Toe/env.py
@@ -18,7 +18,7 @@ class env():
 
             if (len(value_types) == 1) and (value_types[0] != 0):
 
-                print('Winner (Horizontal) : Player {}'.format(value_types[0]))
+                print('Player {} wins (Horizontal)'.format(int(value_types[0])))
 
                 return value_types[0]
                 
@@ -29,14 +29,14 @@ class env():
 
             if (len(value_types) == 1) and (value_types[0] != 0):
 
-                print('Winner (Vertical) : Player {}'.format(value_types[0]))
+                print('Player {} wins (Vertical)'.format(int(value_types[0])))
 
                 return value_types[0]
 
         # Scan for a winner diagonally
         if self.board[0, 0] == self.board[1, 1] == self.board[2, 2] != 0:
 
-            print('Winner (Diagonal) : Player {}'.format(self.board[0, 0]))
+            print('Player {} wins (Diagonal)'.format(int(self.board[0, 0])))
 
             return self.board[0, 0]
 
@@ -49,3 +49,35 @@ class env():
 
         if np.count_nonzero(self.board == 0) == 0: return True
         else: return False
+
+    def step(self, player_num, action):
+
+        reward = 0
+        done = 0
+
+        self.board[action[0], action[1]] = player_num
+
+        next_state = self.board
+
+
+        if self.detect_win() != -1:
+
+            print('There is a winner / Game ends')
+            print(self.board)
+            print('------------------------------')
+            done = 1
+
+        elif self.detect_draw() == False:
+
+            print(self.board)
+            print('------------------------------')
+            done = 0
+
+        elif self.detect_draw() == True:
+
+            print('Board is full / No Winner / Game ends')
+            print(self.board)
+            print('------------------------------')
+            done = 1
+
+        return next_state, reward, done
