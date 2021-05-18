e2e3581b6893ea31a8a2d2324143dc9bfc01a772
luwis93choi <luwis93choi@hotmail.com>
2021-05-19 05:30:29 +0900

    Added plotting for value table

--- 01_Tic_Tac_Toe_[Monte_Carlo]/main.py
+++ 01_Tic_Tac_Toe_[Monte_Carlo]/main.py
@@ -6,48 +6,56 @@ from tqdm import tqdm
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
+import matplotlib.pyplot as plt
+
 RL_player_1 = agent(player_num=1)
 RL_player_2 = agent(player_num=2)
 
 Tic_Tac_Toe_Env = env()
 
-EPISODE = 2000
+EPISODE = 1000
+
+init_state_value_RL_player_1 = np.zeros((EPISODE, len(RL_player_1.action_space)))
 
 for episode in tqdm(range(EPISODE)):
 
     Tic_Tac_Toe_Env.reset()
-
+    
     while True:
 
         # Player 1
-        RL_player_1_action = RL_player_1.act(Tic_Tac_Toe_Env.board)                                             # Player 1 chooses its action based on current state
+        RL_player_1_action = RL_player_1.act(Tic_Tac_Toe_Env.board)     # Player 1 chooses its action based on current state
         
         # print('RL_player_1_action : {}'.format(RL_player_1_action))
         state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_1.player_num, RL_player_1_action)      # Player 1's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state, reward, done for player 1
         RL_player_1.remember(state, next_state, RL_player_1_action, reward, done)
-        
-        if done == 1:
 
-            RL_player_1.update()
-            RL_player_1.memory.clear()
-
-            break
+        if done == 1: break
 
         # Player 2
-        RL_player_2_action = RL_player_2.act(Tic_Tac_Toe_Env.board)                                            # Player 2 chooses its action based on current state
+        RL_player_2_action = RL_player_2.act(Tic_Tac_Toe_Env.board)     # Player 2 chooses its action based on current state
         
         # print('RL_player_2_action : {}'.format(RL_player_2_action))
-        state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_2.player_num, RL_player_2_action)     # Player 2's action interacts with the environment
+        state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_2.player_num, RL_player_2_action)      # Player 2's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state and reward for player 2
         RL_player_2.remember(state, next_state, RL_player_2_action, reward, done)
         
-        if done == 1:
+        if done == 1: break
 
-            RL_player_2.update()
-            RL_player_2.memory.clear()
+    RL_player_1.update()
+    RL_player_1.memory.clear()
 
-            break
+    init_state_value_RL_player_1[episode] = RL_player_1.value_table[-1]
+    # print(init_state_value_RL_player_1)
+    # print(RL_player_1.value_table[-1])
 
-print('{}'.format(RL_player_1.value_table))
+    RL_player_2.update()
+    RL_player_2.memory.clear()
     
+print('RL Player 1 Init State-Action Value : {}'.format(RL_player_1.value_table[-1]))
+print('RL Player 2 Init State-Action Value : {}'.format(RL_player_2.value_table[-1]))
+
+for i in range(len(RL_player_1.action_space)):
+    plt.plot(range(EPISODE), init_state_value_RL_player_1[:, i], '*-')
+plt.show()
