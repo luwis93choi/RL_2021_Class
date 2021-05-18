c866d6810a9c7a9b6c65773f9f5284a609a29ef2
luwis93choi <luwis93choi@hotmail.com>
2021-05-18 17:48:23 +0900

    Added winner detection for termination

--- 01_Tic_Tac_Toe/env.py
+++ 01_Tic_Tac_Toe/env.py
@@ -4,18 +4,43 @@ class env():
 
     def __init__(self):
 
-        self.board = np.zeros((3, 3))
+        self.board = np.zeros((3, 3))   # Shape : height, width
 
     def detect_win(self):
 
-        # Scan for a winner horizontally
-        print(self.board.shape)
+        height = self.board.shape[0]
+        width = self.board.shape[1]
 
+        # Scan for a winner horizontally
+        for i in range(height):
+
+            value_types = np.unique(self.board[i, :])
+
+            if (len(value_types) == 1) and (value_types[0] != 0):
+
+                print('Winner (Horizontal) : Player {}'.format(value_types[0]))
+
+                return value_types[0]
+                
         # Scan for a winner vertically
+        for i in range(height):
+
+            value_types = np.unique(self.board[:, i])
+
+            if (len(value_types) == 1) and (value_types[0] != 0):
+
+                print('Winner (Vertical) : Player {}'.format(value_types[0]))
+
+                return value_types[0]
 
         # Scan for a winner diagonally
+        if self.board[0, 0] == self.board[1, 1] == self.board[2, 2] != 0:
 
-        pass
+            print('Winner (Diagonal) : Player {}'.format(self.board[0, 0]))
+
+            return self.board[0, 0]
+
+        return -1
 
     def detect_draw(self):
 
