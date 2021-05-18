caa5771b3046bbbde427fe5f9bda1a303b375b98
luwis93choi <luwis93choi@hotmail.com>
2021-05-19 01:04:32 +0900

    Added memory function and value table update function

--- 01_Tic_Tac_Toe/agent.py
+++ 01_Tic_Tac_Toe/agent.py
@@ -1,11 +1,30 @@
 import random
 import numpy as np
+from itertools import product
 
 class agent():
 
     def __init__(self, player_num=0):
 
         self.player_num = player_num
+
+        self.discount_factor = 1
+        self.learning_rate = 0.01
+
+        self.grid_height = 3
+        self.grid_width = 3
+
+        # Action space generation
+        self.action_space = [[0, 0], [0, 1], [0, 2], 
+                             [1, 0], [1, 1], [1, 2], 
+                             [2, 0], [2, 1], [2, 2]]
+
+        # State space generation
+        self.state_space = product([2, 1, 0], repeat=self.grid_height * self.grid_width)
+        self.state_space = np.reshape(list(self.state_space), (-1, self.grid_height, self.grid_width))
+                
+        self.value_table = np.zeros((len(self.state_space), len(self.action_space)))     # State (Height), Action Space (Width)
+        self.memory = []
 
     def act(self, board):
 
@@ -29,4 +48,33 @@ class agent():
         
         return [rand_y, rand_x]
 
-    # def update(self, state, )
+    def remember(self, state, next_state, action, reward, done):
+
+        self.memory.append([state, next_state, action, reward, done])
+
+    ### Update Agent's state-action value function ###
+    def update(self):
+
+        G_t = 0
+        visit_states = []
+
+        for sample in reversed(self.memory):
+            state = sample[0]
+            action = sample[2]
+            reward = sample[3]
+
+            for idx in range(len(self.state_space)):
+                if np.array_equal(self.state_space[idx], state):
+                    state_idx = idx
+                    break
+
+            for idx in range(len(self.action_space)):
+                if np.array_equal(self.action_space[idx], action):
+                    action_idx = idx
+                    break
+
+            visit_states.append(state)
+            G_t = reward + self.discount_factor * G_t
+            V_t = self.value_table[state_idx, action_idx]
+
+            self.value_table[state_idx, action_idx] = V_t + self.learning_rate * (G_t - V_t)
