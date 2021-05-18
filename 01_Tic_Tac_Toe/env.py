2f9c7a94e8ce59bdbf2c9645a629ec463dd985e9
luwis93choi <luwis93choi@hotmail.com>
2021-05-19 01:05:03 +0900

    Editted step function to produce full MDP format / Added environment reset

--- 01_Tic_Tac_Toe/env.py
+++ 01_Tic_Tac_Toe/env.py
@@ -1,8 +1,13 @@
 import numpy as np
+import copy
 
 class env():
 
     def __init__(self):
+
+        self.board = np.zeros((3, 3))   # Shape : height, width
+
+    def reset(self):
 
         self.board = np.zeros((3, 3))   # Shape : height, width
 
@@ -18,25 +23,25 @@ class env():
 
             if (len(value_types) == 1) and (value_types[0] != 0):
 
-                print('Player {} wins (Horizontal)'.format(int(value_types[0])))
+                # print('Player {} wins (Horizontal)'.format(int(value_types[0])))
 
                 return value_types[0]
                 
         # Scan for a winner vertically
-        for i in range(height):
+        for i in range(width):
 
             value_types = np.unique(self.board[:, i])
 
             if (len(value_types) == 1) and (value_types[0] != 0):
 
-                print('Player {} wins (Vertical)'.format(int(value_types[0])))
+                # print('Player {} wins (Vertical)'.format(int(value_types[0])))
 
                 return value_types[0]
 
         # Scan for a winner diagonally
         if self.board[0, 0] == self.board[1, 1] == self.board[2, 2] != 0:
 
-            print('Player {} wins (Diagonal)'.format(int(self.board[0, 0])))
+            # print('Player {} wins (Diagonal)'.format(int(self.board[0, 0])))
 
             return self.board[0, 0]
 
@@ -54,30 +59,35 @@ class env():
 
         reward = 0
         done = 0
+        
+        state = copy.deepcopy(self.board)
 
         self.board[action[0], action[1]] = player_num
 
-        next_state = self.board
+        next_state = copy.deepcopy(self.board)
 
 
         if self.detect_win() != -1:
 
-            print('There is a winner / Game ends')
-            print(self.board)
-            print('------------------------------')
+            # print('There is a winner / Game ends')
+            # print(self.board)
+            # print('------------------------------')
             done = 1
+            reward = 1
 
         elif self.detect_draw() == False:
 
-            print(self.board)
-            print('------------------------------')
+            # print(self.board)
+            # print('------------------------------')
             done = 0
+            reward = 0
 
         elif self.detect_draw() == True:
 
-            print('Board is full / No Winner / Game ends')
-            print(self.board)
-            print('------------------------------')
+            # print('Board is full / No Winner / Game ends')
+            # print(self.board)
+            # print('------------------------------')
             done = 1
+            reward = 0
 
-        return next_state, reward, done
+        return state, next_state, reward, done
