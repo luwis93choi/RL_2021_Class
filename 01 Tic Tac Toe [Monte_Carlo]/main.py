07fda55d44102099ab9abcfefa6dc7d68b6d364e
luwis93choi <luwis93choi@hotmail.com>
2021-05-19 22:29:46 +0900

    Added mean error plotting

--- 01 Tic Tac Toe [Monte_Carlo]/main.py
+++ 01 Tic Tac Toe [Monte_Carlo]/main.py
@@ -1,4 +1,3 @@
-from numpy.core.fromnumeric import argmax
 from agent import agent
 from env import env
 
@@ -14,9 +13,13 @@ RL_player_2 = agent(player_num=2)
 
 Tic_Tac_Toe_Env = env()
 
-EPISODE = 2000
+EPISODE = 40000
 
 init_state_value_RL_player_1 = np.zeros((EPISODE, len(RL_player_1.action_space)))
+
+MC_errors = []
+running_average_length = 1000
+running_average = []
 
 for episode in tqdm(range(EPISODE)):
 
@@ -27,7 +30,6 @@ for episode in tqdm(range(EPISODE)):
         # Player 1
         RL_player_1_action = RL_player_1.act(Tic_Tac_Toe_Env.board)     # Player 1 chooses its action based on current state
         
-        # print('RL_player_1_action : {}'.format(RL_player_1_action))
         state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_1.player_num, RL_player_1_action)      # Player 1's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state, reward, done for player 1
         RL_player_1.remember(state, next_state, RL_player_1_action, reward, done)
@@ -37,30 +39,43 @@ for episode in tqdm(range(EPISODE)):
         # Player 2
         RL_player_2_action = RL_player_2.act(Tic_Tac_Toe_Env.board)     # Player 2 chooses its action based on current state
         
-        # print('RL_player_2_action : {}'.format(RL_player_2_action))
         state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_2.player_num, RL_player_2_action)      # Player 2's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state and reward for player 2
         RL_player_2.remember(state, next_state, RL_player_2_action, reward, done)
         
         if done == 1: break
 
-    RL_player_1.update()
+    MC_error_RL_player_1 = RL_player_1.update()
     RL_player_1.memory.clear()
 
     init_state_value_RL_player_1[episode] = RL_player_1.value_table[-1]
-    # print(init_state_value_RL_player_1)
-    # print(RL_player_1.value_table[-1])
 
-    RL_player_2.update()
+    MC_error_RL_player_2 = RL_player_2.update()
     RL_player_2.memory.clear()
     
+    MC_errors.append(MC_error_RL_player_1)
+
+    if episode > running_average_length:
+        running_average.append(np.mean(np.array(MC_errors[-running_average_length:])))
+
 print('RL Player 1 Init State-Action Value : {}'.format(RL_player_1.value_table[-1]))
 
 print('Best First Move at Initial State S0 of Game : {}'.format(RL_player_1.action_space[np.argmax(RL_player_1.value_table[-1])]))
 
+# Plot State-Action value transition at initial state S0
 for i in range(len(RL_player_1.action_space)):
     plt.plot(range(EPISODE), init_state_value_RL_player_1[:, i], '*-', label='Action {}'.format(i))
+
+plt.title('Monte Carlo : State-Action Value at Initial State S0')
 plt.xlabel('Episode')
 plt.ylabel('State-Action Value')
 plt.legend()
+plt.show()
+
+plt.cla()
+
+plt.plot(range(len(running_average)), running_average)
+plt.title('Mean Monte-Carlo Error at Each Episode')
+plt.xlabel('Episode')
+plt.ylabel('Mean Monte-Carlo Error')
 plt.show()
