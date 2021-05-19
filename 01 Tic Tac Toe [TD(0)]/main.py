1ba4e9d4c3b625d457725b18366dc8ad92d57382
luwis93choi <luwis93choi@hotmail.com>
2021-05-19 14:46:43 +0900

    Editted episodic training to support TD(0) settings

--- 01 Tic Tac Toe [TD(0)]/main.py
+++ 01 Tic Tac Toe [TD(0)]/main.py
@@ -14,7 +14,7 @@ RL_player_2 = agent(player_num=2)
 
 Tic_Tac_Toe_Env = env()
 
-EPISODE = 2000
+EPISODE = 10000
 
 init_state_value_RL_player_1 = np.zeros((EPISODE, len(RL_player_1.action_space)))
 
@@ -30,8 +30,9 @@ for episode in tqdm(range(EPISODE)):
         # print('RL_player_1_action : {}'.format(RL_player_1_action))
         state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_1.player_num, RL_player_1_action)      # Player 1's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state, reward, done for player 1
-        RL_player_1.remember(state, next_state, RL_player_1_action, reward, done)
-
+        # print('Player 1 Reward : {}'.format(reward))
+        RL_player_1.update(state, next_state, RL_player_1_action, reward, done)
+        # print(RL_player_1.value_table)
         if done == 1: break
 
         # Player 2
@@ -40,23 +41,18 @@ for episode in tqdm(range(EPISODE)):
         # print('RL_player_2_action : {}'.format(RL_player_2_action))
         state, next_state, reward, done = Tic_Tac_Toe_Env.step(RL_player_2.player_num, RL_player_2_action)      # Player 2's action interacts with the environment
                                                                                                                 # Due to this interaction, environment returns next state and reward for player 2
-        RL_player_2.remember(state, next_state, RL_player_2_action, reward, done)
+        # print('Player 2 Reward : {}'.format(reward))
+        RL_player_2.update(state, next_state, RL_player_2_action, reward, done)
         
         if done == 1: break
 
-    RL_player_1.update()
-    RL_player_1.memory.clear()
-
     init_state_value_RL_player_1[episode] = RL_player_1.value_table[-1]
-    # print(init_state_value_RL_player_1)
-    # print(RL_player_1.value_table[-1])
-
-    RL_player_2.update()
-    RL_player_2.memory.clear()
     
 print('RL Player 1 Init State-Action Value : {}'.format(RL_player_1.value_table[-1]))
 
 print('Best First Move at Initial State S0 of Game : {}'.format(RL_player_1.action_space[np.argmax(RL_player_1.value_table[-1])]))
+
+print(RL_player_1.value_table)
 
 for i in range(len(RL_player_1.action_space)):
     plt.plot(range(EPISODE), init_state_value_RL_player_1[:, i], '*-', label='Action {}'.format(i))
